4f0fa5d3f6af6acc0a1152ac877323f971f09d48
rohan <rohanrajgautam@gmail.com>
2021-03-25 16:42:13 +0545

    added full page text in homepage

--- src/templates/Homepage/Homepage.js
+++ src/templates/Homepage/Homepage.js
@@ -2,13 +2,14 @@ import * as React from "react";
 
 import Layout from "../../layouts";
 import SEO from "../../layouts/common/seo";
-import { Blog, Hero, Footer } from "./components";
+import { Blog, Hero, Footer, FullPageText } from "./components";
 
 const Homepage = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <Hero />
+      <FullPageText />
       <Blog />
       <Footer />
     </Layout>
---
+++ src/templates/Homepage/components/FullPageText/FullPageText.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const FullPageText = () => {
+  return (
+    <TextContainer>
+      <Message>
+        Leading
+        <br />
+        in Wholistic Mental Health
+      </Message>
+    </TextContainer>
+  );
+};
+
+export default FullPageText;
+
+const TextContainer = styled.div`
+  background: #2c3336;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Message = styled.h1`
+  font-family: Merriweather;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 7rem;
+  line-height: 140.5%;
+  text-align: center;
+  letter-spacing: 0.1px;
+  color: #ffffff;
+`;
