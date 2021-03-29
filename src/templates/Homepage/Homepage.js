e52ef1d9e30882630b696fda5773c45ab91cb00c
bbishap <bbishap@gmail.com>
2021-03-29 13:50:30 +0545

    adds newsletter

--- src/templates/Homepage/Homepage.js
+++ src/templates/Homepage/Homepage.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import Layout from "@layouts";
 import SEO from "@layouts/common/seo";
-import { Blog, Hero, Footer, FullPageText } from "./components";
+import { Blog, Hero, Footer, FullPageText, Newsletter } from "./components";
 
 const Homepage = () => {
   return (
@@ -11,6 +11,7 @@ const Homepage = () => {
       <Hero />
       <FullPageText />
       <Blog />
+      <Newsletter />
       <Footer />
     </Layout>
   );
