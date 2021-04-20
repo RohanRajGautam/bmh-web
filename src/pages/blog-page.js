12f9cee68c64f1ba786cd776e6d7aefe91cabaec
bbishap <bbishap@gmail.com>
2021-04-20 08:54:20 +0545

    adds dynamic recent post

--- src/pages/blog-page.js
+++ src/pages/blog-page.js
@@ -1,8 +1,31 @@
 import React from "react";
 import BlogSinglePage from "../templates/BlogSinglePage";
+import { graphql } from "gatsby";
 
 const BlogPage = ({ data }) => {
+  console.log(data);
   return <BlogSinglePage title="Blog page" />;
 };
 
 export default BlogPage;
+
+export const query = graphql`
+  query {
+    allPost: allWpPost {
+      nodes {
+        id
+        uri
+        slug
+        title
+        date
+        excerpt
+        authorId
+        featuredImage {
+          node {
+            sourceUrl
+          }
+        }
+      }
+    }
+  }
+`;
