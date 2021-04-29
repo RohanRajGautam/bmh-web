e6a75093631088fb021e503b58fbd1cc0dec8bcc
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-04-29 12:08:03 +0545

    fixed image width

--- src/templates/Homepage/components/HorizontalScroll/components/FullPageImage/FullPageImage.js
+++ src/templates/Homepage/components/HorizontalScroll/components/FullPageImage/FullPageImage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import image from "@images/home/bmh-long.jpg";
+import image from "@images/home/FullPageImage.png";
 
 // styled components
 
