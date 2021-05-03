9b136a415bc7f95e9fa241594a8bb6a0ce318de9
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-05-03 09:08:04 +0545

    added custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,51 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import styled from "styled-components";
 
 import Layout from "../layouts";
 import Seo from "../layouts/common/seo";
+
+const FourOhFour = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #121212;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const BgImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  mix-blend-mode: overlay;
+
+  background-image: url("http://i.giphy.com/l117HrgEinjIA.gif");
+`;
+const Code = styled.div`
+  font-family: "Mulish", cursive;
+  font-size: 24rem;
+  font-weight: 800;
+  height: 100vh;
+  color: white;
+  width: 100%;
+  display: flex;
+  backgorund-position: center;
+  align-items: center;
+  background-size: cover;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Subtitle = styled.div`
+  font-size: 4rem;
+  margin-top: 4rem;
+`;
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
@@ -10,8 +53,12 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <FourOhFour>
+        <BgImage />
+        <Code>
+          404<Subtitle>Page Not Found</Subtitle>
+        </Code>
+      </FourOhFour>
     </Layout>
   );
 };
