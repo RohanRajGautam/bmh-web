410054254a93c707fd5291bc2bffc63f6b1b8d97
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-06-13 10:35:28 +0545

    added constant for variables

---
+++ src/constants/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const COLORS = {
+  primary: "#1F4CE4",
+  secondary: "#15141a",
+  bgGrey: "#f5f5ef",
+  bgWhite: "#fffff",
+  textHeading: "#15141a",
+  textBody: "#43494D",
+  textDisable: "#B7BCBF",
+  success: "#00B29E",
+  danger: "#E55C64",
+  info: "#E55C64",
+  warning: "#E55C64",
+};
+
+export const SPACING = {
+  xxl: "22rem",
+  xl: "13.6rem",
+  lg: "8.4rem",
+  md: "5.2rem",
+  sm: "3.2rem",
+  xs: "2rem",
+  xxs: "1.2rem",
+};
+
+export const FONT = {
+  h1: "6.8rem",
+  h1H: 1.2,
+  h2: "5rem",
+  h2H: 1.2,
+  h3: "3.7rem",
+  h3H: 1.2,
+  h4: "2.8rem",
+  h4H: 1.4,
+  h5: "2.1rem",
+  h5H: 1.4,
+  h6: "1.6rem",
+  h6H: 1.6,
+
+  p1: "1.5rem",
+  p1H: 1.6,
+  p2: "2rem",
+  p1H: 1.6,
+  p3: "1.3rem",
+  p1H: 1.6,
+  p2: "1.8rem",
+  p1H: 1.6,
+};
