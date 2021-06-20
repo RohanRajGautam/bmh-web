a052759f879ebff679db2fa09f1646da0f86e98d
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-06-20 09:33:05 +0545

    code cleanup and remove unused files

--- src/templates/Homepage/components/HorizontalScroll/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Collage } from "./CollageImage";
-export { default as FullPageImage } from "./FullPageImage";
-export { default as FullPageText } from "./FullPageText";
-export { default as ScrollableFirst } from "./ScrollableFirst";
-export { default as ScrollableLast } from "./ScrollableLast";
--- src/templates/Homepage/components/index.js
+++ src/templates/Homepage/components/index.js
@@ -1,12 +1,8 @@
-export { default as StoryPage } from "./Story";
-export { default as Event } from "./Events";
-export { default as Newsletter } from "./Newsletter";
-export { default as FullPageText } from "./FullPageText";
-// export { default as HorizontalScroll } from "./HorizontalScroll";
-
 export { default as Hero } from "./Hero";
 export { default as DualPage } from "./DualPage";
 export { default as Testimonials } from "./Testimonials";
+export { default as StoryPage } from "./Story";
 export { default as WellnessCampus } from "./WellnessCampus";
 export { default as Blog } from "./Blog";
+export { default as Event } from "./Events";
 export { default as Donate } from "./Donate";
