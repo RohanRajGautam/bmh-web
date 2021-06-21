f28be0546f0f4f7a4666bf824f9635d00523438d
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-06-21 17:09:07 +0545

    seperated big menu for both mobile and desktop

---
+++ src/components/Menu/styled.js
@@ -0,0 +1,199 @@
+import styled, { css } from "styled-components";
+import { SPACING, COLORS } from "@components/constants";
+import { HeadingPrimary } from "@components/Heading";
+import donate from "@images/nav-donate.png";
+import { mediaQueries } from "@components/MediaQueries";
+
+/********************************
+ * BIG MENU ********************
+ ********************************/
+
+export const Wrapper = styled.div``;
+
+export const DonateStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+
+  padding: 5.2rem;
+  height: 100%;
+
+  /* @media (min-width: 768px) {
+    justify-content: flex-end;
+  } */
+`;
+
+export const Heading = styled(HeadingPrimary)`
+  color: #fff;
+`;
+
+export const DonateButtonContainer = styled.div`
+  margin-top: ${SPACING.md};
+`;
+
+export const BigMenu = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  right: 0;
+  top: 0;
+  transform: translateX(100%);
+  overflow: hidden;
+  z-index: 9;
+  background-color: #fff;
+  transition: all 0.8s ease;
+  ${({ open }) =>
+    open &&
+    css`
+      /* height: 100vh;
+     width: 100%; */
+      transition: all 0.8s ease;
+      transform: translateX(0);
+      /* right: 0;
+     top: 0; */
+    `}
+
+  ${mediaQueries("md")`
+   overflow-y: scroll;
+ `}
+`;
+
+export const BigMenuWrapper = styled.div`
+  overflow: hidden;
+  ${({ open }) =>
+    open &&
+    css`
+      height: 100vh;
+      width: 100vw;
+    `}
+`;
+
+export const BigMenuInner = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const BigMenuLeft = styled.div`
+  width: 50rem;
+  height: 100%;
+  background: url(${donate});
+  background-size: cover;
+  background-position: right;
+  background-repeat: no-repeat;
+
+  ${mediaQueries("md")`
+   display: none;
+ `}
+`;
+
+export const BigMenuCenter = styled.div`
+  margin: auto 0;
+`;
+
+export const BigMenuRight = styled.div`
+  width: 13.3rem;
+  border-left: 1px solid ${COLORS.bgGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${mediaQueries("md")`
+   display: none;
+ `}
+`;
+
+export const BigMenuClose = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  margin-bottom: 15rem;
+  /* margin-bottom: 27rem;
+ margin-left: 1rem; */
+
+  svg {
+    path {
+      stroke: ${COLORS.secondary};
+    }
+  }
+
+  &:hover {
+    svg {
+      path {
+        stroke: ${COLORS.primary};
+      }
+    }
+  }
+`;
+
+export const BigMenuNavs = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: ${SPACING.lg};
+  grid-column-gap: ${SPACING.xl};
+  padding: 0 11.6rem;
+
+  @media (max-width: 1500px) {
+    grid-column-gap: 8.4rem;
+    padding: 0 8.4rem;
+  }
+
+  @media (max-width: 1200px) {
+    grid-column-gap: 5.2rem;
+    padding: 0 5.2rem;
+  }
+
+  ${mediaQueries("md")`
+   padding: 0 5vw;
+ `}
+
+  ${mediaQueries("sm")`
+   grid-template-columns: 1fr;
+   margin-top: 8.5rem;
+   padding-top: 3.2rem;
+   padding-left: 3.9rem;
+   padding-bottom: 5.4rem;
+   grid-row-gap: 5.2rem;
+ `}
+`;
+
+export const BigMenuNavTitle = styled.h3`
+  font-family: Merriweather;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 2.1rem;
+  line-height: 3.2rem;
+
+  color: ${COLORS.secondary};
+  margin-bottom: ${SPACING.xs};
+`;
+
+export const BigMenuNavItemWrapper = styled.ul`
+  margin: 0;
+`;
+
+export const BigMenuNavItem = styled.li`
+  list-style: none;
+  font-family: Mulish;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.5rem;
+  line-height: 2.4rem;
+
+  color: #596166;
+
+  &:not(:last-child) {
+    margin-bottom: 1.2rem;
+  }
+  a {
+    color: inherit;
+    font: inherit;
+    text-decoration: none;
+  }
+
+  &:hover {
+    color: ${COLORS.primary};
+  }
+`;
--- src/layouts/common/Sidebar/index.js
+++ src/layouts/common/Sidebar/index.js
@@ -10,32 +10,12 @@ import {
   SmallLogo,
 } from "./svg";
 
-import {
-  Wrapper,
-  Content,
-  Item,
-  DonateStyle,
-  DonateButtonContainer,
-  Heading,
+import { Wrapper, Content, Item } from "./styled";
 
-  //big_menu
-  BigMenu,
-  BigMenuInner,
-  BigMenuLeft,
-  BigMenuCenter,
-  BigMenuNavs,
-  BigMenuNavTitle,
-  BigMenuNavItemWrapper,
-  BigMenuNavItem,
-  BigMenuRight,
-  BigMenuClose,
-} from "./styled";
-
-import { staticData } from "../Navbar/Navbar.datas";
-import DonateBtn from "../../../templates/Homepage/components/Donate/DonateBtn";
+import BigMenuComponent from "@components/Menu";
 
 const Sidebar = () => {
-  const [bigMenu, setBigMenu] = React.useState(false);
+  const [bigMenu, setBigMenu] = useState(false);
 
   const handleBigMenu = e => {
     e.preventDefault();
@@ -98,75 +78,7 @@ const Sidebar = () => {
           <SmallLogo />
         </Item>
       </Content>
-      <BigMenu open={bigMenu}>
-        <BigMenuInner>
-          <BigMenuLeft>
-            <DonateStyle>
-              <Heading>
-                Your support
-                <br /> can <span>change</span>
-                <br />
-                someone’s life
-              </Heading>
-              <DonateButtonContainer>
-                <DonateBtn to="https://beautifulmindswellness.org/donate/" />
-              </DonateButtonContainer>
-            </DonateStyle>
-          </BigMenuLeft>
-          <BigMenuCenter>
-            <BigMenuNavs>
-              {staticData.length &&
-                staticData.map((item, index) => (
-                  <div key={index}>
-                    <BigMenuNavTitle>{item.title}</BigMenuNavTitle>
-                    <BigMenuNavItemWrapper>
-                      {item.items.length &&
-                        item.items.map((item, index) => (
-                          <BigMenuNavItem key={index}>
-                            <a
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              href={item.to}
-                              // onClick={handleBigMenu}
-                            >
-                              {item.title}
-                            </a>
-                          </BigMenuNavItem>
-                        ))}
-                    </BigMenuNavItemWrapper>
-                  </div>
-                ))}
-            </BigMenuNavs>
-          </BigMenuCenter>
-
-          <BigMenuRight>
-            <BigMenuClose onClick={handleBigMenu}>
-              <svg
-                width="61"
-                height="46"
-                viewBox="0 0 61 46"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17 41L42.4978 15.5022"
-                  stroke="#15141A"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M17 16L42.4978 41.4978"
-                  stroke="#15141A"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </BigMenuClose>
-          </BigMenuRight>
-        </BigMenuInner>
-      </BigMenu>
+      <BigMenuComponent handleBigMenu={handleBigMenu} bigMenu={bigMenu} />
     </Wrapper>
   );
 };
