b7f82b45270ca5055390928d1f19a59515c6d546
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-07-02 17:41:25 +0545

    read more page design fix

--- src/templates/OurStory/index.js
+++ src/templates/OurStory/index.js
@@ -2,43 +2,70 @@ import React from "react";
 import styled from "styled-components";
 import Layout from "@layouts";
 import Seo from "@layouts/common/seo";
-import { Heading } from "@components/Heading";
+import { HeadingPrimary } from "@components/Heading";
 
 import { Container } from "@components/Container";
 import Image from "@components/Image";
 import { data } from "./data";
+
+import FullLogoAlt from "@images/icons/full-logo-alt";
+
 
 const CoverImage = styled.div`
   width: 100vw;
   height: auto;
   overflow: hidden;
 
-  @media (max-width: 576px) {
+  position: relative;
+
+  @media (max-width: 960px) {
     width: 180vw;
   }
 `;
 
 const DescImage = styled.div`
-  width: 50%;
   height: auto;
-  margin: 3rem 0 3rem 5rem;
+    width: 100%;
+
+    padding: 5.2rem 0;
+
+  @media (min-width: 860px) {
+  width: 50%;
+    padding: 0 0 5.2rem 5.2rem;
   float: right;
 
-  @media (max-width: 860px) {
-    width: 100%;
   }
 `;
 
-const Wrapper = styled(Container)`
+const Wrapper = styled.div`
   height: auto;
   padding: 5vh 5vw;
+  max-width: 140rem;
+  margin: 0 auto;
 `;
-const Title = styled(Heading)`
-  font-size: 6.4rem;
-  color: #6ca448;
+const Title = styled(HeadingPrimary)`
+  /* H2 */
+  font-family: Merriweather;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 6.8rem;
+  line-height: 8.2rem;
+
+  span {
+    font-size: inherit;
+    line-height: inherit;
+  }
+
+  /* Text/Hd-Grey-01 */
+  color: #15141a;
 
   @media (max-width: 576px) {
-    font-size: 4.4rem;
+    font-size: 5rem;
+  line-height: 1.2;
+
+  
+
+    
   }
 `;
 
@@ -53,26 +80,47 @@ const Description = styled.div`
   margin-top: 4rem;
 `;
 const Desc = styled.p`
+  padding-bottom: 5.2rem;
+  text-align-last: initial;
+
   font-family: Mulish;
   font-style: normal;
-  font-weight: 600;
-  font-size: 2.4rem;
+  font-weight: normal;
+  font-size: 2rem;
   line-height: 160%;
-  letter-spacing: 0.1px;
-  color: #5f6468;
-  padding-bottom: 2rem;
+
+  display: flex;
+  align-items: center;
+
+  /* Text/ Body-Grey-02 */
+  color: #596166;
+`;
+
+const LogoWrapper = styled.div`
+  a{
+    position: absolute;
+    top: 3vh;
+    left: 5vw;
+    z-index: 1;
+  }
 `;
 
 const OurStory = () => {
   return (
     <Layout dark>
       <Seo title="Our Story" />
+      <Container>
       <CoverImage>
-        <Image src="ourstory-bg.jpg" />
+          <LogoWrapper>
+          <FullLogoAlt to="/" />
+        </LogoWrapper>
+        <Image src="ourstory-bg.png" />
       </CoverImage>
       <Divider />
       <Wrapper>
-        <Title>{data.title}</Title>
+        <Title>
+          Our Story <span>meets</span> Yours
+        </Title>
         <Description>
           <Desc>{data.desc1}</Desc>
           <Desc>{data.desc2}</Desc>
@@ -83,6 +131,7 @@ const OurStory = () => {
           <Desc>{data.desc4}</Desc>
         </Description>
       </Wrapper>
+      </Container>
     </Layout>
   );
 };
