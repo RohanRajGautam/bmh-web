3d310c332711844f01f449d259aacee55121dc7e
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-07-04 20:13:54 +0545

    fix wellness-campus page timeline issue

--- src/templates/WellnessCampus/components/Timeline.jsx
+++ src/templates/WellnessCampus/components/Timeline.jsx
@@ -47,7 +47,7 @@ const Item = styled.li`
 
 const Top = styled.div`
   color: #15141a;
-  padding-bottom: 5rem;
+  padding-bottom: 8rem;
   position: relative;
 
   &::after {
@@ -82,7 +82,16 @@ const Top = styled.div`
     text-align: right;
     color: inherit;
   }
+
+  @media (min-width: 1085px) {
+    padding-bottom: 6rem;
+  }
+
+  @media (min-width: 1265px) {
+    padding-bottom: 4rem;
+  }
 `;
+
 const Bottom = styled.div`
   padding-top: 5rem;
   position: relative;
@@ -132,7 +141,9 @@ const LastItem = styled.li`
 `;
 
 const LastTop = styled.div`
-  padding-bottom: 3rem;
+  color: #15141a;
+  padding-bottom: 5rem;
+  position: relative;
 
   h4 {
     font-family: Merriweather;
@@ -141,12 +152,24 @@ const LastTop = styled.div`
     font-weight: 700;
     line-height: 3.9rem;
     letter-spacing: 0em;
-    text-align: center;
+    text-align: right;
+    color: inherit;
+  }
+
+  p {
+    font-family: Mulish;
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.4rem;
+    letter-spacing: 0em;
+    text-align: right;
+    color: inherit;
   }
 `;
 const LastBottom = styled.div`
   width: 7rem;
-  padding-top: 3rem;
+  padding-top: 5rem;
 
   p {
     font-family: Mulish;
@@ -162,14 +185,44 @@ const LastBottom = styled.div`
 const MiddleLogo = styled.div`
   background: ${COLORS.primary};
   border-radius: 50%;
-  width: 61px;
-  height: 61px;
+  width: 41px;
+  height: 41px;
+
+  position: absolute;
+  top: 38%;
 
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1;
+
+  @media (min-width: 1085px) {
+    top: 36%;
+  }
+
+  @media (min-width: 1265px) {
+    top: 35%;
+  }
 `;
+
+const BhmWhiteLogo = () => (
+  <svg
+    width="27"
+    height="24"
+    viewBox="0 0 27 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.0965 11.6457C13.3488 12.1353 13.4957 12.7863 13.5483 13.4894C14.2157 12.9269 15.8444 12.6197 17.1318 13.1301C15.776 13.1301 14.0948 13.854 13.5483 14.6092C13.4485 16.0935 12.9495 17.5622 12.1035 18.1143C15.7814 18.6872 17.2632 13.8332 20.3579 14.2134C19.1284 11.2135 14.678 10.5729 13.0965 11.6457Z"
+      fill="white"
+    />
+    <path
+      d="M10.2077 4.23424C5.86777 4.77067 2.27408 12.6715 5.48432 23.1972C5.72592 23.9992 6.34078 23.9107 7.43884 23.7961C13.5652 23.1555 25.2976 21.9055 26.0122 13.9735C26.4641 8.96847 22.4655 5.38524 17.5581 5.06756C17.4426 5.06232 17.3482 4.97902 17.2956 4.88004C16.5443 3.44781 13.8384 0 10.1238 0C-0.75758 0 -1.06756 17.9734 1.95874 22.8013C2.66289 23.9211 3.69257 23.7961 3.42979 23.0565C0.692553 15.3953 3.06216 2.38013 10.3968 2.38013C12.7454 2.38013 14.3742 3.82799 15.9611 6.8227C16.0241 6.94768 16.1606 6.99457 16.2973 7.01019C21.6197 7.70808 23.737 11.1455 23.4061 14.0048C23.0382 17.213 20.6265 20.6556 7.23907 22.1347C6.59284 20.8587 5.11133 12.8017 7.49649 8.98929C6.39843 13.635 7.49649 17.7911 8.40041 19.2598C11.1744 18.1505 12.2043 16.8953 12.2043 14.1037C12.2043 9.98925 8.38969 7.63516 10.2077 4.23424Z"
+      fill="white"
+    />
+  </svg>
+);
 
 const Timeline = () => {
   return (
@@ -237,11 +290,15 @@ const Timeline = () => {
             </Bottom>
           </Item>
           <LastItem>
-            <LastTop>
+            {/* <LastTop>
               <h4>Future</h4>
+            </LastTop> */}
+            <LastTop>
+              <h4>2024</h4>
+              <p>April,</p>
             </LastTop>
             <MiddleLogo>
-              <img src={logoXs} alt="logo" />
+              <BhmWhiteLogo />
             </MiddleLogo>
             <LastBottom>
               <p>Complete Wellness Campus!</p>
