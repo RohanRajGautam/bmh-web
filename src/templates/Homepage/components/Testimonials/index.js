afe5d02ffde65ceb9a3eba50d95be8b3013aca4e
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-07-06 10:57:26 +0545

    add testimonial video in homepage

--- src/templates/Homepage/components/Testimonials/index.js
+++ src/templates/Homepage/components/Testimonials/index.js
@@ -12,29 +12,50 @@ import {
 import { HeadingPrimary, HeadingSecondary } from "@components/Heading";
 import PlayButton from "@components/SvgComponent/play-button";
 import Dots from "@components/SvgComponent/dots";
+import VideoModal from "@components/VideoModal";
 
 const Testimonials = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <MainContainer>
-      {/* <ContentWrapper > */}
-      <BackgroundVector>
-        <RellaxWrapper speed={5} percentage={0.3}>
-          <Dots />
-        </RellaxWrapper>
-      </BackgroundVector>
-      <Header data-aos="fade-up">
-        <HeadingSecondary>TESTIMONIALS</HeadingSecondary>
-        <HeadingPrimary>
-          Stories of <span> true</span> <br />
-          hope and healing
-        </HeadingPrimary>
-      </Header>
-      <Cta>
-        <PlayButton />
-      </Cta>
-      <People></People>
-      {/* </ContentWrapper> */}
-    </MainContainer>
+    <>
+      <MainContainer>
+        {/* <ContentWrapper > */}
+        <BackgroundVector>
+          <RellaxWrapper speed={5} percentage={0.3}>
+            <Dots />
+          </RellaxWrapper>
+        </BackgroundVector>
+        <Header data-aos="fade-up">
+          <HeadingSecondary>TESTIMONIALS</HeadingSecondary>
+          <HeadingPrimary>
+            Stories of <span> true</span> <br />
+            hope and healing
+          </HeadingPrimary>
+        </Header>
+        <Cta>
+          <div onClick={handleClickOpen}>
+            <PlayButton />
+          </div>
+        </Cta>
+        <People></People>
+        {/* </ContentWrapper> */}
+      </MainContainer>
+      <VideoModal
+        channel="youtube"
+        videoId="7na2JlF_OQI"
+        open={open}
+        handleClose={handleClose}
+      />
+    </>
   );
 };
 
