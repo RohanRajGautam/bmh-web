4be94b8ef001d86fd1e8a9ffc99810f3c73579dc
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-07-11 12:27:57 +0545

    optimized sidebar code

--- src/layouts/common/Sidebar/index.js
+++ src/layouts/common/Sidebar/index.js
@@ -1,16 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import {
-  Medical,
-  Wellness,
-  Menu,
-  Blog,
-  Events,
-  Donate,
-  SmallLogo,
-} from "./svg";
+import { Menu, Donate, SmallLogo, RightArrow } from "./svg";
 
-import { Wrapper, Content, Item } from "./styled";
+import { Wrapper, Content, Item, ItemWrapper, Text } from "./styled";
 
 import BigMenuComponent from "@components/Menu";
 
@@ -44,42 +36,55 @@ const Sidebar = () => {
   return (
     <Wrapper>
       <Content>
-        <Item
-          href="https://www.beautifulmindsmedical.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          clicked={bigMenu}
-        >
-          <Medical />
-        </Item>
-        <Item
-          href="https://beautifulmindswellness.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          clicked={bigMenu}
-        >
-          <Wellness />
-        </Item>
-        <Item onClick={handleBigMenu} clicked={bigMenu}>
-          <Menu bigMenu={bigMenu} />
-        </Item>
-        <Item href="/blog-cat" clicked={bigMenu}>
-          <Blog />
-        </Item>
-        <Item href="/#events" clicked={bigMenu}>
-          <Events />
-        </Item>
-        <Item
-          href="https://beautifulmindswellness.org/donate/"
-          target="_blank"
-          rel="noopener noreferrer"
-          clicked={bigMenu}
-        >
-          <Donate />
-        </Item>
-        <Item className="bmh-logo-sidebar" href="/" clicked={bigMenu}>
-          <SmallLogo />
-        </Item>
+        <ItemWrapper clicked={bigMenu}>
+          <Item
+            href="https://www.beautifulmindsmedical.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Text>Medical</Text>
+            <RightArrow />
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item
+            href="https://beautifulmindswellness.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Text>Wellness</Text>
+            <RightArrow />
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item onClick={handleBigMenu}>
+            <Menu bigMenu={bigMenu} />
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item href="/blog-cat">
+            <Text>Blog</Text>
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item href="/#events">
+            <Text>Events</Text>
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item
+            href="https://beautifulmindswellness.org/donate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Donate />
+          </Item>
+        </ItemWrapper>
+        <ItemWrapper clicked={bigMenu}>
+          <Item className="bmh-logo-sidebar" href="/">
+            <SmallLogo />
+          </Item>
+        </ItemWrapper>
       </Content>
       <BigMenuComponent handleBigMenu={handleBigMenu} bigMenu={bigMenu} />
     </Wrapper>
