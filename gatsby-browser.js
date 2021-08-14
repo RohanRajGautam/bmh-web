e722265cb0f3d42de29ce5e1d76422971314993c
Rohan Gautam <rohanrajgautam@gmail.com>
2021-08-14 22:56:52 +0545

    Add service worker updates message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,16 @@ import "typeface-merriweather";
 // Highlighting for code blocks
 import "prismjs/themes/prism.css";
 import "react-modal-video/scss/modal-video.scss";
+
+// Displaying a message when a service worker updates
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
+
+export const registerServiceWorker = () => true;
